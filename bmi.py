b7cc9dd0233f79eebbf515069afecf4f1cb1617a
how <eonghowfoh@gmail.com>
2023-11-27 15:13:27 +0800

    retuern value

--- bmi.py
+++ bmi.py
@@ -8,9 +8,12 @@ def calculate_bmi(height, weight):
 
     if bmi<18.5:
         print("weight classification: Under weight")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("Weight Classification: Normal Weight")
+        return 0
     else:
         print("Weight Classification: Over Weight")
+        return 1
 
 calculate_bmi(weight=50, height=1.60)
--- lab2.py
+++ lab2.py
@@ -1,5 +1,39 @@
 import statistics
 
+def display_main_menu():
+    print("Enter some numbers separated by spaces (e.g. 5 67 32)")  # Changed commas to spaces for input format
+
+def calc_average(numlist):
+    total = 0.0
+    for x in numlist:
+        total += x
+    average = total / len(numlist)
+    return average
+
+def calc_average_temperature(numlist):  # New function to calculate average temperature
+    return statistics.mean(numlist)
+
+def get_user_input():
+    templist = input()
+    splitlist = templist.split()  # Changed split delimiter to spaces
+    floatlist = [float(x) for x in splitlist]
+    return floatlist
+
+def find_min_max(numlist):
+    x = ["Minimum value = " + str(min(numlist)), "Maximum value = " + str(max(numlist))]
+    return x
+
+def calc_min_max_temperature(numlist):  # New function to calculate min and max temperature
+    return [min(numlist), max(numlist)]
+
+def sort_temperature(numlist):
+    numlist.sort()
+    return numlist
+
+def calc_median_temperature(numlist):
+    median = statistics.median(numlist)
+    return median
+
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
@@ -13,34 +47,6 @@ def main():
     print("Sorted values: " + str(tempsort))
     mediantemp = calc_median_temperature(numlist)
     print("Median Value: " + str(mediantemp))
-
-def display_main_menu():
-    print("Enter some numbers separated by spaces (e.g. 5 67 32)")  # Changed commas to spaces for input format
-
-def calc_average(numlist):
-    total = 0.0
-    for x in numlist:
-        total += x
-    average = total / len(numlist)
-    return average
-
-def get_user_input():
-    templist = input()
-    splitlist = templist.split()  # Changed split delimiter to spaces
-    floatlist = [float(x) for x in splitlist]
-    return floatlist
-
-def find_min_max(numlist):
-    x = ["Minimum value = " + str(min(numlist)), "Maximum value = " + str(max(numlist))]
-    return x
-
-def sort_temperature(numlist):
-    numlist.sort()
-    return numlist
-
-def calc_median_temperature(numlist):
-    median = statistics.median(numlist)
-    return median
 
 if __name__ == "__main__":
     main()
